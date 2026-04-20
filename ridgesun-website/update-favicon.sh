be027adfb8dfe93ce52eaf845730728c1df5f471
Minjie Sun <minjiesun@MinjiedeMac-mini.local>
2026-04-20 13:09:13 +0800

    Fix: Remove broken file references and update favicon to data URI

---
+++ ridgesun-website/update-favicon.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Simple blue square favicon as data URI
+FAVICON_DATA='data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMzIiIGhlaWdodD0iMzIiIHZpZXdCb3g9IjAgMCAzMiAzMiIgZmlsbD0ibm9uZSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4KPHJlY3Qgd2lkdGg9IjMyIiBoZWlnaHQ9IjMyIiBmaWxsPSIjMWUzYThhIi8+Cjx0ZXh0IHg9IjE2IiB5PSIyMCIgZm9udC1mYW1pbHk9IkFyaWFsIiBmb250LXNpemU9IjE0IiBmaWxsPSJ3aGl0ZSIgdGV4dC1hbmNob3I9Im1pZGRsZSI+UjwvdGV4dD4KPC9zdmc+'
+
+cd src
+
+for file in *.html; do
+    echo "Updating $file"
+    # Replace favicon.ico with data URI
+    sed -i '' "s|href=\"/assets/favicon.ico\"|href=\"$FAVICON_DATA\"|g" "$file"
+done
+
+echo "Favicon updates complete!"
